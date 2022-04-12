59327e93a665da5dd5c31bbdcd2f6f793ec747cc
jj <jotajota1302@hotmail.com>
2022-04-12 18:33:35 +0200

    commit

---
+++ relacionesjpa/src/main/java/edu/es/eoi/Application.java
@@ -0,0 +1,10 @@
+package edu.es.eoi;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ relacionesjpa/src/test/java/edu/es/eoi/ApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.es.eoi;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ApplicationTests {
+
+	@Test
+	public void test() {
+	
+		assertEquals(1,1);
+		
+	}
+
+}

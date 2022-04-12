5fed5a98dcaed8eecf96e9b4af07b0df9c74ac4b
jj <jotajota1302@hotmail.com>
2022-04-12 18:59:01 +0200

    commit

--- relacionesjpa/src/main/java/edu/es/eoi/Application.java
+++ relacionesjpa/src/main/java/edu/es/eoi/Application.java
@@ -3,7 +3,13 @@ package edu.es.eoi;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
+		
+		//objetivo: saber el nombre del presidente del equipo X
+		
+		//Scanner, introduce id equipo y la respuesta tiene que ser el nombre del presidente
+		
+		
 
 	}
 
---
+++ relacionesjpa/src/main/java/edu/es/eoi/EquipoRepository.java
@@ -0,0 +1,12 @@
+package edu.es.eoi;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class EquipoRepository {
+	
+	public EntityManager entityManager = Persistence.createEntityManagerFactory("BECA_PU").createEntityManager();
+	
+	//CRUD
+
+}

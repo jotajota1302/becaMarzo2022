161733786a8040e345efed498a6436a277af1d8c
jj <jotajota1302@hotmail.com>
2022-04-12 16:30:06 +0200

    commit

--- jpaejemplo/src/main/java/edu/es/eoi/repository/UserRepository.java
+++ jpaejemplo/src/main/java/edu/es/eoi/repository/UserRepository.java
@@ -6,59 +6,57 @@ import javax.persistence.Persistence;
 import edu.es.eoi.entity.User;
 
 public class UserRepository {
-	
-	//CRUD (Create Read Update y Delete)
-	
-	public EntityManager entityManager=Persistence.createEntityManagerFactory("BECA_PU").createEntityManager();
-	
-	public User readUser(int id) {
-		
-		return entityManager.find(User.class,id);
-		
-	}
-	
-	public void createUser(User user) {
-		
-		entityManager.getTransaction().begin();
-		
-		entityManager.persist(user);
-		
-		entityManager.getTransaction().commit();
-	}
-	
-	//update user
-	
 
+	// CRUD (Create Read Update y Delete)
+
+	public EntityManager entityManager = Persistence.createEntityManagerFactory("BECA_PU").createEntityManager();
+
+	//Create
+	public void createUser(User user) {
+
+		entityManager.getTransaction().begin();
+
+		entityManager.persist(user);
+
+		entityManager.getTransaction().commit();
+	}
+
+	// Read
+	public User readUser(int id) {
+
+		return entityManager.find(User.class, id);
+
+	}
+
+	//Update
 	public void updateUser(User user) {
-		
+
 		entityManager.getTransaction().begin();
-		
+
 		entityManager.merge(user);
-		
+
 		entityManager.getTransaction().commit();
 	}
-	
-	
-	//delete user
+
+	//Delete
 	public void deleteUser(User user) {
-		
+
 		entityManager.getTransaction().begin();
-		
+
 		entityManager.remove(user);
-		
+
 		entityManager.getTransaction().commit();
 	}
-	
+
 	public void deleteUserById(int id) {
-		
-		User user=readUser(id);
-		
+
+		User user = readUser(id);
+
 		entityManager.getTransaction().begin();
-		
+
 		entityManager.remove(user);
-		
+
 		entityManager.getTransaction().commit();
 	}
-	
 
 }

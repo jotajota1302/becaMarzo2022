b06f27f0f6e18fada3d47b44a66143e901b36303
jj <jotajota1302@hotmail.com>
2022-04-26 16:22:04 +0200

    commit

--- myfirstcontroller/src/main/java/edu/es/eoi/myfirstcontroller/MyController.java
+++ myfirstcontroller/src/main/java/edu/es/eoi/myfirstcontroller/MyController.java
@@ -1,21 +1,23 @@
 package edu.es.eoi.myfirstcontroller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/*")
+@RequestMapping("/home")
 public class MyController {
 
-	@RequestMapping("home")
-	public @ResponseBody String home() {
-		return "hello world";
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody String getHome() {
+		return "hello world from get";
 	}
-
-	@RequestMapping("index")
-	public String index() {
-		return "index.html";
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody String postHome() {
+		return "hello world from post";
 	}
 
 }

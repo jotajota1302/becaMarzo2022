30070ea85352c7e072109f1f66f19e37a3a69d5f
jj <jotajota1302@hotmail.com>
2022-04-26 16:44:24 +0200

    commit

--- myfirstcontroller/src/main/java/edu/es/eoi/myfirstcontroller/MyController.java
+++ myfirstcontroller/src/main/java/edu/es/eoi/myfirstcontroller/MyController.java
@@ -3,6 +3,7 @@ package edu.es.eoi.myfirstcontroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,13 +11,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MyController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody String getHome() {
-		return "hello world from get";
+	public @ResponseBody String getHome(@RequestParam String nombre,@RequestParam String apellidos) {
+		return "hello world from get, you are: " + nombre+", " + apellidos;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody String postHome() {
 		return "hello world from post";
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	public @ResponseBody String putHome() {
+		return "hello world from put";
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE)
+	public @ResponseBody String deleteHome() {
+		return "hello world from delete";
 	}
 
 }

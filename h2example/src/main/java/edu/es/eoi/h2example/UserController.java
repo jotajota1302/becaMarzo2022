6119e77b6e6ec84d47a913486e50217df2444834
jj <jotajota1302@hotmail.com>
2022-04-27 17:04:02 +0200

    commit

--- h2example/src/main/java/edu/es/eoi/h2example/UserController.java
+++ h2example/src/main/java/edu/es/eoi/h2example/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,9 +30,23 @@ public class UserController {
 
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-
 		return ResponseEntity.ok(service.findAll());
 
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/saluda1")
+	public String saluda() {
+		return "hello";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/saluda2")
+	public ResponseEntity<String> saludaEntity() {
+		return new ResponseEntity<>("hello", HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/saluda3")
+	public ResponseEntity<String> getNombre(@RequestParam(value = "name") String name) {
+		return new ResponseEntity<>("hello".concat(" ").concat(name), HttpStatus.OK);
 	}
 
 }
